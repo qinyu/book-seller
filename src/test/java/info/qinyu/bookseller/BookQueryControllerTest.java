6d3415bd703fb496394eb8f479dd3dbfba5ddb34
qinyu <qinyusuain@gmail.com>
2019-03-05 11:12:09 +0800

    Usage of ParameterizedTest and MethodSource

--- src/test/java/info/qinyu/bookseller/BookQueryControllerTest.java
+++ src/test/java/info/qinyu/bookseller/BookQueryControllerTest.java
@@ -2,14 +2,16 @@ package info.qinyu.bookseller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static info.qinyu.bookseller.Books.*;
 import static java.util.Arrays.asList;
-import static java.util.Collections.*;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -29,31 +31,19 @@ class BookQueryControllerTest {
         bookQueryController = new BookQueryController(bookRepository);
     }
 
-    @Test
-    @DisplayName("查到了两本DDD")
-    void should_return_book_list_when_querying_book() {
+    @ParameterizedTest(name = "查询{0}得到{1}本书")
+    @MethodSource("createQueryArguments")
+    void should_return_book_list_when_querying_name(String name, int expectedSize, List<Book> expectedBooks) {
+        List<Book> bookList = bookQueryController.queryBookByName(name);
 
-        List<Book> bookList = bookQueryController.queryBookByName("DDD");
-
-        assertThat(bookList).hasSize(2).contains(IDDD, DDDD);
+        assertThat(bookList).hasSize(expectedSize).containsExactlyElementsOf(expectedBooks);
     }
 
-    @Test
-    @DisplayName("查到了一本DDD")
-    void should_return_book_list_when_querying_return_only_one_book() {
-
-        List<Book> bookList = bookQueryController.queryBookByName("Koltin");
-
-        assertThat(bookList).hasSize(1).contains(KIA);
+    private static Stream<Arguments> createQueryArguments() {
+        return Stream.of(
+                Arguments.of("DDD", 2, asList(IDDD, DDDD)),
+                Arguments.of("Koltin", 1, singletonList(KIA)),
+                Arguments.of("Spring", 0, emptyList())
+        );
     }
-
-    @Test
-    @DisplayName("没有查到")
-    void should_return_empty_list_when_querying_not_existing_book() {
-
-        List<Book> bookList = bookQueryController.queryBookByName("Spring");
-
-        assertThat(bookList).hasSize(0);
-    }
-
 }
